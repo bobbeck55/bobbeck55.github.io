931d6b760c0a2a5da4b8005eda767ce3e19813fb
bobbeck55 <bob_beck@sil.org>
2022-10-31 12:59:31 -0400

    Deployed 2861218 with MkDocs version: 1.4.1

---
+++ transcriber/end_to_end.js
@@ -0,0 +1,49 @@
+
+function xhr_test(){
+
+   //month = document.getElementById('month').value;
+   alert("hello world");
+   
+   const xhr = new XMLHttpRequest();
+   
+   alert("after xhr new");
+   
+   //const url = `https://api.github.com/users/bobbeck55/repos`;
+   const url = `https://api.github.com/users/bobbeck55/repo/bobbeck55.github.io`;
+   
+   alert("after url");
+	  
+   xhr.open('GET', url, true);
+   
+   alert("after open");
+   
+   xhr.onload = function() {
+    
+	    alert("onload function");
+		
+        // Parse API data into JSON
+        const data = JSON.parse(this.response);
+        		
+        alert("after json parse");
+		
+        // Log the response
+		
+		for (let i in data) {
+
+		   str = "repo: " + data[i].name;
+		   alert(str);
+		   
+		   str = "description: " + data[i].description;
+		   alert(str);
+		   
+		   str = "url: " + data[i].html_url;
+		   alert(str);
+		   
+		}
+   }
+   alert("after onload");
+   
+   xhr.send();
+   
+   alert("after send");
+}
